01bec35eea69602cd9c07b8a74043d546e14b09f
Nelio Alves <jefferson2112@hotmail.com>
2020-03-06 12:28:39 -0300

    APP

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -36,7 +36,15 @@ function Main() {
   return (
     <MapView initialRegion={currentRegion} style={styles.map}>
       <Marker coordinate={{ latitude: -8.0448238, longitude: -34.8829916 }}>
-        <Image source={{ uri: 'https://avatars1.githubusercontent.com/u/2254731?s=400&v=4' }} />
+        <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/2254731?s=400&v=4' }} />
+
+        <Callout>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Diego Fernandes</Text>
+            <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
+            <Text style={styles.devTechs}>React.js, React Native, Node.js</Text>
+          </View>
+        </Callout>
       </Marker>
     </MapView >
   );
@@ -45,6 +53,31 @@ function Main() {
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 250
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5
+  },
+
+  devTechs: {
+    marginTop: 5
   }
 })
 
